aa023a20f80e2f970e12e5b449f2d3834bc07f77
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-22 23:33:28 +0200

    setting up the core data model and the data controller class which is going to load that data from the core data

---
+++ BookWorm/DataController.swift
@@ -0,0 +1,14 @@
+//
+//  DataController.swift
+//  BookWorm
+//
+//  Created by Buhle Radzilani on 2024/06/22.
+//
+
+import CoreData
+import Foundation
+
+
+class DataController: ObservableObject{
+    
+}
