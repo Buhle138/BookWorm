b22831c784fe77d9cae909b44af1d357173962e7
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-22 23:02:02 +0200

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Buhle Radzilani on 2024/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
