6a91100e3c960db7d7c748d92e65e8b77daf0fd8
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-23 10:47:52 +0200

    creating the star rating view.

---
+++ RatingView.swift
@@ -0,0 +1,31 @@
+//
+//  RatingView.swift
+//  BookWorm
+//
+//  Created by Buhle Radzilani on 2024/06/23.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
+}
