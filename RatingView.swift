cfe2b587657fbdfda06758527db54156989929df
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-23 10:56:22 +0200

    finished result of the rating image.

--- RatingView.swift
+++ RatingView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+
+//LOGIC
+//if the number that was passed in is greater than the current rating then return the off image otherwise return the on image
+//
+
 struct RatingView: View {
     @Binding var rating: Int
     
@@ -20,12 +25,33 @@ struct RatingView: View {
     var onColor = Color.yellow
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1, id: \.self){number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+            }
+        }
     }
+    func image(for number: Int) -> Image{
+        if number > rating{
+            return offImage ?? onImage
+        }else {
+            return onImage
+        }
+    }
+    
+    
 }
 
 struct RatingView_Previews: PreviewProvider {
     static var previews: some View {
-        RatingView()
+        RatingView(rating: .constant(4))
     }
 }
