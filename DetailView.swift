605723f211916c152f9fbc790a4b823fac61ea38
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-23 11:33:06 +0200

    showing all the book details

--- DetailView.swift
+++ DetailView.swift
@@ -14,9 +14,33 @@ struct DetailView: View {
         ScrollView{
             ZStack(alignment: .bottomTrailing) {
                 Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
                 
             }
+            
+            Text(book.author ?? "Unknown Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+            
         }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
